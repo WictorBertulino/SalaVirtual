81f9dd5f090b48073c5e0430b1f26b403b6c28e2
wictorbertulino <wictorbertulino@gmail.com>
2019-11-05 10:49:02 -0400

    ajuste de conteudos

--- application/views/sistema/Conteudos.php
+++ application/views/sistema/Conteudos.php
@@ -75,7 +75,7 @@
      <div class="col-md-12">
             <div class="card">
               <div class="card-header">
-                <h4 class="card-title"> Professores</h4>
+                <h4 class="card-title"> Conteudos</h4>
               </div>
               <div class="card-body">
               <?php if ($this->session->flashdata("NovoProfessorSucesso")) { ?>
