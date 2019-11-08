2b61d196220244bed08eb9698648052ef824249e
wictorbertulino <wictorbertulino@gmail.com>
2019-11-08 17:35:27 -0400

    Adicionando download

--- application/views/sistema/Professores.php
+++ application/views/sistema/Professores.php
@@ -11,12 +11,12 @@
         Tip 1: You can change the color of the sidebar using: data-color="blue | green | orange | red | yellow"
     -->
       <div class="logo">
-        <a href="http://www.creative-tim.com" class="simple-text logo-mini">
+        <a href="/index.php/Home" class="simple-text logo-mini">
           <div class="logo-image-small">
             <img src="<?=base_url()?>/assets/img/logo-small.png">
           </div>
         </a>
-        <a href="http://www.creative-tim.com" class="simple-text logo-normal">
+        <a href="/index.php/Home" class="simple-text logo-normal">
          Sala Virtual
           <!-- <div class="logo-image-big">
             <img src="../assets/img/logo-big.png">
