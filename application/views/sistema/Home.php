82633db643a24cd9136f2cc71899d4c42dd5aad6
wictorbertulino <wictorbertulino@gmail.com>
2019-11-08 17:33:15 -0400

    Adicionando download

--- application/views/sistema/Home.php
+++ application/views/sistema/Home.php
@@ -28,6 +28,7 @@
       </div>
     </div>
     <div class="main-panel">
+    
       <!-- Navbar -->
       <nav class="navbar navbar-expand-lg navbar-absolute fixed-top navbar-transparent">
         <div class="container-fluid">
