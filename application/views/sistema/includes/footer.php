ed424532901ea5ee54dcda6520f235ebb37e896a
wictorbertulino <wictorbertulino>
2020-05-20 10:18:21 -0400

    implementação de chat

--- application/views/sistema/includes/footer.php
+++ application/views/sistema/includes/footer.php
@@ -4,7 +4,6 @@
   <script src="<?=base_url()?>/assets/js/core/bootstrap.min.js"></script>
   <script src="<?=base_url()?>/assets/js/plugins/perfect-scrollbar.jquery.min.js"></script>
   <!--  Google Maps Plugin    -->
-  <script src="https://maps.googleapis.com/maps/api/js?key=YOUR_KEY_HERE"></script>
   <!-- Chart JS -->
   <script src="<?=base_url()?>/assets/js/plugins/chartjs.min.js"></script>
   <!--  Notifications Plugin    -->
