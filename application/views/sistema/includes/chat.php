c9499b7d4001ee4a30abf98d0a8586e067148027
wictorbertulino <wictorbertulino>
2020-05-20 11:57:02 -0400

    implementação de chat

--- application/views/sistema/includes/chat.php
+++ application/views/sistema/includes/chat.php
@@ -1,7 +1,7 @@
 <button class="open-button" onclick="openForm()">Discussão</button>
 
 <div class="chat-popup" id="myForm">
-  <div  class="form-container">
+  <form  class="form-container" onsubmit="enviarData()">
     <h1>Chat</h1>
     <input style="display: none" id="name" value="<?=$this->session->userdata('nameUserSession')?>">
     <ul id="listMessage">
@@ -12,9 +12,9 @@
     <label for="msg"><b>Message</b></label>
     <input placeholder="Digite sua mensagem" id="msg" name="msg" required></input>
 
-    <button onclick="enviarData()" class="btn">Enviar Mensagem</button>
+    <button type="submit" class="btn">Enviar Mensagem</button>
     <button type="button" class="btn cancel" onclick="closeForm()">Fechar</button>
-</div>
+</form>
 </div>
 <script src="https://cdnjs.cloudflare.com/ajax/libs/socket.io/2.3.0/socket.io.dev.js" integrity="sha256-OgiHfllOBEOiWQ2OcBsDncRG2k7QBW9Rs1dB7MH2N2s=" crossorigin="anonymous"></script>
 <script>
@@ -29,7 +29,8 @@
   let socket = io('http://localhost:3000')
 
 
-  function enviarData() {
+  function enviarData(event) {
+    event.preventDefault();
     let obj = {
       Nome: document.getElementById("name").value,
       Message:document.getElementById("msg").value
