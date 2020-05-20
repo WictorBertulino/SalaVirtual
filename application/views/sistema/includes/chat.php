871508188d0e9c556e3427e80e119c2c4fd30069
wictorbertulino <wictorbertulino>
2020-05-20 10:59:43 -0400

    implementação de chat

--- application/views/sistema/includes/chat.php
+++ application/views/sistema/includes/chat.php
@@ -1,89 +1,24 @@
 
-<div class="popup-box chat-popup 'popup-box-on'" id="qnimate">
-    		  <div class="popup-head">
-					  <div class="popup-head-right pull-right">
-				
-						<button data-widget="remove" id="removeClass" class="chat-header-button pull-right" type="button"><i class="glyphicon glyphicon-off"></i></button>
-                      </div>
-			  </div>
-			<div class="popup-messages">
-    		
-			
-			
-			
-			<div class="direct-chat-messages">
-                    
-					
-					<div class="chat-box-single-line">
-								<abbr class="timestamp">October 8th, 2015</abbr>
-					</div>
-					
-					
-					<!-- Message. Default to the left -->
-                    <div class="direct-chat-msg doted-border">
-                      <div class="direct-chat-info clearfix">
-                        <span class="direct-chat-name pull-left">Osahan</span>
-                      </div>
-                      <!-- /.direct-chat-info -->
-                      <img alt="message user image" src="http://bootsnipp.com/img/avatars/bcf1c0d13e5500875fdd5a7e8ad9752ee16e7462.jpg" class="direct-chat-img"><!-- /.direct-chat-img -->
-                      <div class="direct-chat-text">
-                        Hey bro, how’s everything going ?
-                      </div>
-					  <div class="direct-chat-info clearfix">
-                        <span class="direct-chat-timestamp pull-right">3.36 PM</span>
-                      </div>
-						<div class="direct-chat-info clearfix">
-						<span class="direct-chat-img-reply-small pull-left">
-							
-						</span>
-						<span class="direct-chat-reply-name">Singh</span>
-						</div>
-                      <!-- /.direct-chat-text -->
-                    </div>
-                    <!-- /.direct-chat-msg -->
-					
-					
-					<div class="chat-box-single-line">
-						<abbr class="timestamp">October 9th, 2015</abbr>
-					</div>
-			
-					
-					
-					<!-- Message. Default to the left -->
-                    <div class="direct-chat-msg doted-border">
-                      <div class="direct-chat-info clearfix">
-                        <span class="direct-chat-name pull-left">Osahan</span>
-                      </div>
-                      <!-- /.direct-chat-info -->
-                      <img alt="iamgurdeeposahan" src="http://bootsnipp.com/img/avatars/bcf1c0d13e5500875fdd5a7e8ad9752ee16e7462.jpg" class="direct-chat-img"><!-- /.direct-chat-img -->
-                      <div class="direct-chat-text">
-                        Hey bro, how’s everything going ?
-                      </div>
-					  <div class="direct-chat-info clearfix">
-                        <span class="direct-chat-timestamp pull-right">3.36 PM</span>
-                      </div>
-						<div class="direct-chat-info clearfix">
-						  <img alt="iamgurdeeposahan" src="http://bootsnipp.com/img/avatars/bcf1c0d13e5500875fdd5a7e8ad9752ee16e7462.jpg" class="direct-chat-img big-round">
-						<span class="direct-chat-reply-name">Singh</span>
-						</div>
-                      <!-- /.direct-chat-text -->
-                    </div>
-                    <!-- /.direct-chat-msg -->
-					
-					
-                    
+<button class="open-button" onclick="openForm()">Chat</button>
 
-                    
+<div class="chat-popup" id="myForm">
+  <form action="/action_page.php" class="form-container">
+    <h1>Chat</h1>
 
-                  </div>
-<script src="https://cdnjs.cloudflare.com/ajax/libs/socket.io/2.3.0/socket.io.dev.js"></script>
+    <label for="msg"><b>Message</b></label>
+    <textarea placeholder="Type message.." name="msg" required></textarea>
+
+    <button type="submit" class="btn">Send</button>
+    <button type="button" class="btn cancel" onclick="closeForm()">Close</button>
+  </form>
+</div>
+
 <script>
-   
-  let socket = io('http://localhost:3000')
+function openForm() {
+  document.getElementById("myForm").style.display = "block";
+}
 
-
-  function enviarData(){
-  let obj = {Nome:"João Wictor", Message:'Olá mundo'}
-  socket.emit('sendMessage',obj);
-  }
+function closeForm() {
+  document.getElementById("myForm").style.display = "none";
+}
 </script>
