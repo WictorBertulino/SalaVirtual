cf4ec91404a362baabbabbb8f5926bb46a20bb40
wictorbertulino <wictorbertulino>
2020-05-20 11:23:11 -0400

    implementação de chat

--- application/views/sistema/includes/chat.php
+++ application/views/sistema/includes/chat.php
@@ -1,16 +1,15 @@
-
 <button class="open-button" onclick="openForm()">Discussão</button>
 
 <div class="chat-popup" id="myForm">
   <form action="/action_page.php" class="form-container">
     <h1>Chat</h1>
-    <ul >
+    <ul>
       <li>Wictor : Olá mundo</li>
       <li>João : Olá tudo bem</li>
-      
+
     </ul>
     <label for="msg"><b>Message</b></label>
-    <textarea placeholder="Digite sua mensagem" name="msg" required></textarea>
+    <textarea placeholder="Digite sua mensagem" id="msg" name="msg" required></textarea>
 
     <button type="submit" class="btn">Enviar Mensagem</button>
     <button type="button" class="btn cancel" onclick="closeForm()">Fechar</button>
@@ -18,11 +17,22 @@
 </div>
 
 <script>
-function openForm() {
-  document.getElementById("myForm").style.display = "block";
-}
+  function openForm() {
+    document.getElementById("myForm").style.display = "block";
+  }
 
-function closeForm() {
-  document.getElementById("myForm").style.display = "none";
-}
+  function closeForm() {
+    document.getElementById("myForm").style.display = "none";
+  }
+
+  let socket = io('http://localhost:3000')
+
+
+  function enviarData() {
+    let obj = {
+      Nome: "João Wictor",
+      Message:document.getElementById("id").value
+    }
+    socket.emit('sendMessage', obj);
+  }
 </script>
